be2504531de9f48e168ddaa2747382c697847e62
Beatriz Vidal <biavidalf@gmail.com>
2022-08-25 10:06:44 -0300

    Adicionando algoritmo de num primo em JS

---
+++ Atividades/Entregavel1/Javascript/main.js
@@ -0,0 +1,10 @@
+/*
+  ENTREGÁVEL 1 - AMBIENTES DE DESENVOLVIMENTO DE SOFTWARE
+  Beatriz Vidal, 2217226, ADS
+  Algoritmos fundamentais em Java
+*/
+import { numeroPrimo } from "./primo.js";
+/*
+* ================= ALGORITMO 1 =================
+*/
+console.log(numeroPrimo(5));
